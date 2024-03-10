a036afa8eb55422491fa15d4e8343a99c52f089a
FerRMZ <zs22013690@estudiantes.uv.mx>
2024-03-09 21:34:41 -0600

    Actualizacion del dominio

--- CoilJava/src/Logica/Dominio/EstudianteExterno.java
+++ CoilJava/src/Logica/Dominio/EstudianteExterno.java
@@ -1,4 +1,22 @@
 package Logica.Dominio;
 
 public class EstudianteExterno {
+    private String matricula;
+    private Facultad facultad;
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Facultad getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(Facultad facultad) {
+        this.facultad = facultad;
+    }
 }
