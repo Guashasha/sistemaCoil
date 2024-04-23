b456d04c1814b7a647e326b04d115eefc3244741
guashasha <davidcarrionr00@hotmail.com>
2024-04-23 15:55:05 -0600

    primera version de estilos de ventanas, primera version de DAO cronograma de actividades

--- CoilJava/src/InterfazGrafica/Controlador/AgregarActividadesControlador.java
+++ CoilJava/src/InterfazGrafica/Controlador/AgregarActividadesControlador.java
@@ -1,5 +1,39 @@
 package InterfazGrafica.Controlador;
 
-public class AgregarActividadesControlador {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.log4j.Logger;
 
+import java.io.IOException;
+
+public class AgregarActividadesControlador extends Application {
+    public static final Logger BITACORA = Logger.getLogger(NuevaActividadControlador.class);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Parent root = null;
+
+        try {
+            root = FXMLLoader.load(getClass().getResource("Plantilla/agregarActividades.fxml"));
+        }
+        catch (IOException e) {
+            BITACORA.error(e);
+        }
+
+        if (root != null) {
+            stage.setTitle("Crear actividad nueva");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        else {
+            BITACORA.error("Ocurrió un error al iniciar la ventana windowNuevaActividad");
+        }
+    }
 }
--- CoilJava/src/InterfazGrafica/Controlador/NuevaActividadControlador.java
+++ CoilJava/src/InterfazGrafica/Controlador/NuevaActividadControlador.java
@@ -1,15 +1,31 @@
 package InterfazGrafica.Controlador;
 
 import Logica.DAO.DAOActividad;
+import Logica.DAO.DAOCronogramaActividades;
 import Logica.Dominio.Actividad;
+import Logica.Dominio.ActividadVinculada;
+import Logica.Dominio.Colaboracion;
 import Logica.Dominio.Periodo;
 import Logica.ErrorDAO;
+import javafx.application.Application;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.apache.log4j.Logger;
 
+import java.io.IOException;
 import java.time.LocalDate;
 
-public class NuevaActividadControlador {
+public class NuevaActividadControlador extends Application {
+    private static final Logger BITACORA = Logger.getLogger(NuevaActividadControlador.class);
+
+    private Colaboracion colaboracion;
+
     @FXML
     private Button btnAceptar = new Button();
     @FXML
@@ -24,6 +40,44 @@ public class NuevaActividadControlador {
     private TextField tfDescripcion = new TextField();
     @FXML
     private TextField tfTitulo = new TextField();
+
+    public void setColaboracion (Colaboracion colaboracion) {
+        if (colaboracion.validarNulos()) {
+            this.colaboracion = colaboracion;
+        }
+        else {
+            cerrarVentana();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Parent root = null;
+
+        try {
+            root = FXMLLoader.load(getClass().getResource("../Plantilla/NuevaActividad.fxml"));
+        }
+        catch (IOException e) {
+            BITACORA.error(e);
+        }
+
+        if (root != null) {
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setTitle("Crear actividad nueva");
+
+            Scene escena = new Scene(root, Color.TRANSPARENT);
+            escena.getStylesheets().add("InterfazGrafica/Estilos/ventana.css");
+
+            stage.setScene(escena);
+            stage.show();
+        } else {
+            BITACORA.error("Ocurrió un error al iniciar la ventana windowNuevaActividad");
+        }
+    }
 
     private boolean camposInvalidos () {
         return tfTitulo.getText()
@@ -66,49 +120,70 @@ public class NuevaActividadControlador {
             resultado = dao.agregar(actividad);
         }
         catch (ErrorDAO error) {
-            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
-
-            switch (error.getTipo()) {
-                case VALIDACION:
-                    errorAlert.setHeaderText("Error de datos");
-                    errorAlert.setContentText("Los datos de la actividad son incorrectos, verifiquelos e intente de nuevo");
-                    break;
-
-                case CONSULTA:
-                    errorAlert.setHeaderText("Error al agregar");
-                    errorAlert.setContentText("Ocurrió un error al agregar la actividad");
-                    break;
-
-                case DUPLICIDAD:
-                    errorAlert.setHeaderText("La actividad ya existe");
-                    errorAlert.setContentText("Ya existe una actividad con estos datos, intente de nuevo");
-                    break;
-
-                case CONEXION:
-                    errorAlert.setHeaderText("Error de base de datos");
-                    errorAlert.setContentText("Ocurrió un error al conectarse a la base de datos, intente nuevamente más tarde");
-                    break;
-            }
-
+            Alert errorAlert = crearAlerta(error);
             errorAlert.showAndWait();
 
             return;
         }
 
-        if (resultado > 0) {
-            Alert errorAlert = new Alert(Alert.AlertType.INFORMATION);
-            errorAlert.setHeaderText("Actividad agregada correctamente");
-            errorAlert.setContentText("La actividad se encuentra ahora en su cronograma de actividades");
-            errorAlert.showAndWait();
-        } else {
+        if (resultado < 1) {
             Alert errorAlert = new Alert(Alert.AlertType.ERROR);
             errorAlert.setHeaderText("Error al agregar");
             errorAlert.setContentText("Ocurrió un error al agregar la actividad");
             errorAlert.showAndWait();
         }
+
+        ActividadVinculada actividadVinculada = new ActividadVinculada(actividad, colaboracion, periodo);
+        DAOCronogramaActividades cronograma = new DAOCronogramaActividades();
+
+        try {
+            resultado = cronograma.agregar(actividadVinculada);
+        }
+        catch (ErrorDAO error) {
+            Alert errorAlert = crearAlerta(error);
+            errorAlert.showAndWait();
+
+            return;
+        }
+
+        if (resultado < 1) {
+            Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+            errorAlert.setHeaderText("Error al agregar");
+            errorAlert.setContentText("Ocurrió un error al vincular la actividad");
+            errorAlert.showAndWait();
+        }
     }
 
     public void cerrarVentana () {
+        Stage window = (Stage) btnCancelar.getScene().getWindow();
+        window.close();
+    }
 
+    private static Alert crearAlerta (ErrorDAO error) {
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR);
+
+        switch (error.getTipo()) {
+            case VALIDACION:
+                errorAlert.setHeaderText("Error de datos");
+                errorAlert.setContentText("Los datos de la actividad son incorrectos, verifiquelos e intente de nuevo");
+                break;
+
+            case CONSULTA:
+                errorAlert.setHeaderText("Error al agregar");
+                errorAlert.setContentText("Ocurrió un error al agregar la actividad");
+                break;
+
+            case DUPLICIDAD:
+                errorAlert.setHeaderText("La actividad ya existe");
+                errorAlert.setContentText("Ya existe una actividad con estos datos, intente de nuevo");
+                break;
+
+            case CONEXION:
+                errorAlert.setHeaderText("Error de base de datos");
+                errorAlert.setContentText("Ocurrió un error al conectarse a la base de datos, intente nuevamente más tarde");
+                break;
+        }
+
+        return errorAlert;
     }
 }
