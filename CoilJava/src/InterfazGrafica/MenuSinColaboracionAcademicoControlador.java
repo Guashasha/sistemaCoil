c1552162afe92c1fcae86ffcf1fee9f33dbfad4f
guashasha <davidcarrionr00@hotmail.com>
2024-05-04 22:18:38 -0600

    cambios a ventana principal y terminado inicio academico

---
+++ CoilJava/src/InterfazGrafica/MenuSinColaboracionAcademicoControlador.java
@@ -0,0 +1,16 @@
+package InterfazGrafica;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MenuSinColaboracionAcademicoControlador extends Application {
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start (Stage primaryStage) {
+
+    }
+}
