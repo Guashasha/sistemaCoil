7bca8b91df5ce922cea1000262f0d71fb92f711b
Pale04 <epalemolina@gmail.com>
2024-05-12 12:07:03 -0600

    Procedimientos numeralia

--- vistas_numeralia.sql
+++ vistas_numeralia.sql
@@ -1,27 +1,34 @@
-CREATE VIEW colaboraciones_uv_finalizadas AS
-SELECT idColaboracion, cedulaProfesional, estado, nombre, nombreUniversidad, areaEstudios,
-nombreFacultad, nombreRegion
-FROM academicodesarrolla AS ad LEFT JOIN vista_academico AS va
+CREATE VIEW colaboraciones_uv_finalizadas AS 
+SELECT ad.idColaboracion, cedulaProfesional, ad.estado, nombre, nombreUniversidad, areaEstudios,
+nombreFacultad, nombreRegion, fechaFin, COUNT(idEstudiante) AS alumnosUvTotales
+FROM academicodesarrolla AS ad 
+LEFT JOIN vista_academico AS va
 ON ad.idAcademico = va.cedulaProfesional
+LEFT JOIN colaboracion AS c
+ON ad.idColaboracion = c.idColaboracion
+LEFT JOIN estudiantescolaboracion AS ec
+ON ad.idColaboracion = ec.idColaboracion
 WHERE va.nombreUniversidad = 'Universidad Veracruzana' 
 AND (ad.estado = 'anfitrion' OR ad.estado = 'aceptado')
-AND idColaboracion IN (SELECT colaboracion.idColaboracion FROM colaboracion WHERE estado = 'finalizada');
-
-
-CREATE VIEW estudiantes_uv_colaboracion AS
-SELECT idColaboracion, COUNT(idEstudiante) AS alumnosUvTotales 
-FROM estudiantescolaboracion
-WHERE idEstudiante IN 
-(SELECT idEstudiante FROM vista_estudiante WHERE universidad IN 
-(SELECT universidad.idUniversidad FROM universidad WHERE nombre = 'Universidad Veracruzana'))
+AND idEstudiante IN 
+(SELECT idEstudiante FROM vista_estudiante WHERE universidad IN (SELECT universidad.idUniversidad FROM universidad WHERE nombre = 'Universidad Veracruzana'))
+AND ad.idColaboracion IN (SELECT colaboracion.idColaboracion FROM colaboracion WHERE estado = 'finalizada') 
 GROUP BY idColaboracion;
 
 
 CREATE VIEW numeralia AS 
-SELECT cuf.idColaboracion, areaEstudios AS areaAcademica, nombreFacultad AS facultad, 
-nombreRegion AS region, alumnosUvTotales
-FROM 
-colaboraciones_uv_finalizadas AS cuf 
-LEFT JOIN 
-estudiantes_uv_colaboracion AS euc
-ON cuf.idColaboracion = euc.idColaboracion;
+SELECT ad.idColaboracion, areaEstudios AS areaAcademica,
+nombreFacultad AS facultad, nombreRegion AS region, fechaFin, COUNT(idEstudiante) AS alumnosUvTotales
+FROM academicodesarrolla AS ad 
+LEFT JOIN vista_academico AS va
+ON ad.idAcademico = va.cedulaProfesional
+LEFT JOIN colaboracion AS c
+ON ad.idColaboracion = c.idColaboracion
+LEFT JOIN estudiantescolaboracion AS ec
+ON ad.idColaboracion = ec.idColaboracion
+WHERE va.nombreUniversidad = 'Universidad Veracruzana' 
+AND (ad.estado = 'anfitrion' OR ad.estado = 'aceptado')
+AND idEstudiante IN 
+(SELECT idEstudiante FROM vista_estudiante WHERE universidad IN (SELECT universidad.idUniversidad FROM universidad WHERE nombre = 'Universidad Veracruzana'))
+AND ad.idColaboracion IN (SELECT colaboracion.idColaboracion FROM colaboracion WHERE estado = 'finalizada') 
+GROUP BY idColaboracion;
