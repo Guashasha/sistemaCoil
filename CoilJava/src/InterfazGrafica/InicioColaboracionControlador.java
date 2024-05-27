836377e93b20843510512579a93b7d774aca8f96
FerRMZ <zs22013690@estudiantes.uv.mx>
2024-05-27 17:24:44 -0600

    Avances

---
+++ CoilJava/src/InterfazGrafica/InicioColaboracionControlador.java
@@ -0,0 +1,14 @@
+package InterfazGrafica;
+
+import DAO.AcademicoAuxiliar;
+import DAO.AcademicoDAO;
+import DTO.ColaboracionDTO;
+
+public class InicioColaboracionControlador {
+    private ColaboracionDTO colaboracionDTO;
+
+
+    public void setColaboracionDTO (ColaboracionDTO colaboracionDTO) {
+        this.colaboracionDTO = colaboracionDTO;
+    }
+}
