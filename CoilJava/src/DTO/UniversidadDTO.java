e07484823cd3ad57d5f1cf0b91648754b6962eed
Pale04 <epalemolina@gmail.com>
2024-06-09 17:41:51 -0600

    Test completos para EstudianteAuxiliar y EstudianteDAO

--- CoilJava/src/DTO/UniversidadDTO.java
+++ CoilJava/src/DTO/UniversidadDTO.java
@@ -3,29 +3,65 @@ package DTO;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * La clase UniversidadDTO funciona como transfer object, para transferir la información desde la base de datos a capas
+ * superiores dentro de la aplicación.
+ * @author pale
+ */
 public class UniversidadDTO {
+    /**
+     * Id con el que está registrado la universidad en la base de datos.
+     */
     private int id;
+    /**
+     * Nombre de la universidad.
+     */
     private String nombre;
+    /**
+     * Id del pais al que pertenece la universidad.
+     */
     private int idPais;
+    /**
+     * Límite de longitud del nombre de la universidad para ser registrada en la base de datos.
+     */
     public static final int LONGITUD_NOMBRE = 50;
 
     public UniversidadDTO() {
 
     }
 
+    /**
+     * inicializa la universidad solamente con su id
+     * @param id id con el que está registrado la universidad en la base de datos
+     */
     public UniversidadDTO(int id) {
         this.id = id;
     }
 
+    /**
+     * Inicializa la universidad solamente con su nombre
+     * @param nombre nombre de la universidad
+     */
     public UniversidadDTO(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Inicializa la universidad con su nombre y el id del país al cual pertenece.
+     * @param nombre nombre de la universidad
+     * @param idPais id del pais al que pertenece la universidad
+     */
     public UniversidadDTO(String nombre, int idPais) {
         this.nombre = nombre;
         this.idPais = idPais;
     }
 
+    /**
+     * Inicializa la universidad con su id, su nombre y el id del pais al cual pertenece.
+     * @param id id de la universidad.
+     * @param nombre nombre de la universidad.
+     * @param idPais id del país al cual pertenece la universidad.
+     */
     public UniversidadDTO(int id, String nombre, int idPais) {
         this.id = id;
         this.nombre = nombre;
@@ -56,6 +92,10 @@ public class UniversidadDTO {
         this.idPais = idPais;
     }
 
+    /**
+     * Determina que el nombre de la universidad esté inicializado y que tenga un formato válido.
+     * @return true si el nombre está inicializado y en formato válido, de otra manera false.
+     */
     public boolean nombreValido () {
         boolean nombreValido;
         if (this.nombre == null || this.nombre.isBlank()) {
