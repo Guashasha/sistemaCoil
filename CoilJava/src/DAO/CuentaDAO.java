3303bb6717b4082da04de4822723a1c3481cc110
FerRMZ <zs22013690@estudiantes.uv.mx>
2024-06-10 07:31:41 -0600

    Documentacion del api cuentaDAO y estudianteDAO

--- CoilJava/src/DAO/CuentaDAO.java
+++ CoilJava/src/DAO/CuentaDAO.java
@@ -16,6 +16,13 @@ import java.util.Optional;
 public class CuentaDAO implements ICuentaDAO {
     private static final Logger BITACORA = Logger.getLogger(CuentaDAO.class);
 
+    /**
+     * Obtiene una cuenta basada en el nombre de usuario.
+     *
+     * @param nombreUsuario el nombre de usuario de la cuenta que se desea obtener.
+     * @return un objeto Optional que contiene la cuenta encontrada o vacío si no se encuentra la cuenta.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public Optional<CuentaDTO> getCuentaPorUsuario (String nombreUsuario) throws ErrorDAO {
         String cuentaPorUsuarioSQL = "SELECT * from cuenta WHERE nombreUsuario = ?";
@@ -42,6 +49,13 @@ public class CuentaDAO implements ICuentaDAO {
         return Optional.ofNullable(cuentaDTO);
     }
 
+    /**
+     * Actualiza el nombre de usuario de una cuenta.
+     *
+     * @param cuentaDTO el objeto CuentaDTO que contiene la información de la cuenta a actualizar.
+     * @return el número de filas afectadas por la actualización.
+     * @throws ErrorDAO si ocurre un error durante la actualización en la base de datos.
+     */
     @Override
     public int actualizarNombreUsuario (CuentaDTO cuentaDTO) throws ErrorDAO {
         String actualizarUsuarioSQL = "UPDATE cuenta SET nombreUsuario = ? WHERE idCuenta = ?";
@@ -66,6 +80,14 @@ public class CuentaDAO implements ICuentaDAO {
         return  filasAfectadas;
     }
 
+    /**
+     * Verifica las credenciales de una cuenta y obtiene un booleano como parametro sálida.
+     *
+     * @param nombreUsuario el nombre de usuario.
+     * @param contrasena la contraseña.
+     * @return true si las credenciales son válidas, false en caso contrario.
+     * @throws ErrorDAO si ocurre un error durante la verificación en la base de datos.
+     */
     @Override
     public boolean verificarCredenciales (String nombreUsuario, String contrasena) throws ErrorDAO {
         String verificarCredencialesSQL = "{CALL verificar_credenciales(?, ?, ?)}";
@@ -92,6 +114,15 @@ public class CuentaDAO implements ICuentaDAO {
         return validacion;
     }
 
+    /**
+     * Actualiza la contraseña de una cuenta por medio de un procedimiento almacenado.
+     *
+     * @param cuentaDTO el objeto CuentaDTO que contiene la información de la cuenta a actualizar.
+     * @param contrasenaAntigua la contraseña antigua.
+     * @param contrasenaNueva la nueva contraseña.
+     * @return el número de filas afectadas por la actualización.
+     * @throws ErrorDAO si ocurre un error durante la actualización en la base de datos.
+     */
     @Override
     public int actualizarContrasena (CuentaDTO cuentaDTO, String contrasenaAntigua, String contrasenaNueva) throws ErrorDAO {
         String actualizarContrasenaSQL = "{CALL cambiar_contrasena(?,?,?,?)}";
@@ -118,6 +149,14 @@ public class CuentaDAO implements ICuentaDAO {
         return filasAfectadas;
     }
 
+    /**
+     * Cambia el estado de una cuenta.
+     *
+     * @param cuentaDTO el objeto CuentaDTO que contiene la información de la cuenta con el estado al que se desea actualizar y su identificador.
+     * @param estado el nuevo estado de la cuenta.
+     * @return el número de filas afectadas por la actualización.
+     * @throws ErrorDAO si ocurre un error durante la actualización en la base de datos.
+     */
     @Override
     public int cambiarEstadoCuenta (CuentaDTO cuentaDTO, String estado) throws ErrorDAO {
         String cambiarEstadoCuentaSQL = "UPDATE cuenta SET estado = ? WHERE idCuenta = ?";
@@ -143,6 +182,13 @@ public class CuentaDAO implements ICuentaDAO {
         return filasAfectadas;
     }
 
+    /**
+     * Obtiene una lista de cuentas basadas en su tipo.
+     *
+     * @param tipo el tipo de las cuentas que se desean obtener.
+     * @return una lista de objetos CuentaDTO que cumplen con el criterio especificado.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public List<CuentaDTO> getCuentasPorTipo (String tipo) throws ErrorDAO {
         String getCuentaPorTipoSQL = "SELECT * FROM cuenta WHERE tipo = ?";
@@ -171,6 +217,13 @@ public class CuentaDAO implements ICuentaDAO {
         return listaCuentaDTOS;
     }
 
+    /**
+     * Obtiene una lista de cuentas basadas en su estado en el que se encuentran registradas en la base de datos.
+     *
+     * @param estado el estado de las cuentas que se desean obtener.
+     * @return una lista de objetos CuentaDTO que cumplen con el criterio especificado.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public List<CuentaDTO> getCuentasPorEstado (String estado) throws ErrorDAO {
         String getCuentasPorEstadoSQL = "SELECT * FROM cuenta WHERE estado = ?";
@@ -200,6 +253,13 @@ public class CuentaDAO implements ICuentaDAO {
         return listaCuentaDTOS;
     }
 
+    /**
+     * Agrega una nueva cuenta a la base de datos.
+     *
+     * @param cuentaDTO el objeto CuentaDTO que contiene la información de la cuenta a agregar.
+     * @return el número de filas afectadas por la inserción.
+     * @throws ErrorDAO si ocurre un error durante la inserción en la base de datos.
+     */
     @Override
     public int agregar (CuentaDTO cuentaDTO) throws ErrorDAO {
         String agregarCuentaSQL = "{CALL registrar_cuenta(?,?,?,?,?)}";
@@ -227,11 +287,26 @@ public class CuentaDAO implements ICuentaDAO {
         return filasAfectadas;
     }
 
+    /**
+     * Modifica una cuenta existente en la base de datos.
+     *
+     * @param obj el objeto CuentaDTO que contiene la información de la cuenta a modificar.
+     * @return el número de filas afectadas por la modificación.
+     * @throws ErrorDAO si ocurre un error durante la modificación en la base de datos.
+     * @throws ExecutionControl.NotImplementedException si el método no está implementado.
+     */
     @Override
     public int modificar (CuentaDTO obj) throws ErrorDAO, ExecutionControl.NotImplementedException {
         throw new ExecutionControl.NotImplementedException("Metodo no implementado");
     }
 
+    /**
+     * Obtiene una cuenta basada en su ID.
+     *
+     * @param id el ID de la cuenta que se desea obtener.
+     * @return un objeto Optional que contiene la cuenta encontrada o vacío si no se encuentra la cuenta.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public Optional<CuentaDTO> getPorId (Integer id) throws ErrorDAO {
         String getPorIdSQL = "SELECT * from cuenta WHERE idCuenta = ?";
@@ -259,6 +334,13 @@ public class CuentaDAO implements ICuentaDAO {
         return Optional.ofNullable(cuentaDTO);
     }
 
+    /**
+     * Obtiene una cuenta basada en el ID de la persona asociada.
+     *
+     * @param idPersona el ID de la persona asociada a la cuenta que se desea obtener.
+     * @return un objeto Optional que contiene la cuenta encontrada o vacío si no se encuentra la cuenta.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public Optional<CuentaDTO> getCuentaPorPersona (int idPersona) throws ErrorDAO {
         CuentaDTO cuenta = null;
@@ -282,6 +364,12 @@ public class CuentaDAO implements ICuentaDAO {
         return Optional.ofNullable(cuenta);
     }
 
+    /**
+     * Obtiene todas las cuentas registradas en la base de datos.
+     *
+     * @return una lista de todos los objetos CuentaDTO registrados en la base de datos.
+     * @throws ErrorDAO si ocurre un error durante la consulta a la base de datos.
+     */
     @Override
     public List<CuentaDTO> getTodos () throws ErrorDAO {
         String getTodosSQL = "SELECT * from cuenta";
