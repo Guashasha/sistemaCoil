fff74f22a2584d0d8d06e94687ddf60b564fc086
FerRMZ <zs22013690@estudiantes.uv.mx>
2024-06-18 11:08:12 -0600

    meow

--- CoilJava/src/InterfazGrafica/ProgresoColaboracionControlador.java
+++ CoilJava/src/InterfazGrafica/ProgresoColaboracionControlador.java
@@ -77,10 +77,8 @@ public class ProgresoColaboracionControlador {
         getAcademicoParPorColaboracion();
         cargarLabels();
         actualizarVisibilidadBotones();
-        if (colaboracionDTO.getEstado() == ColaboracionDTO.EstadoColaboracion.enRevision) {
-            RetroalimentacionColaboracionAuxiliar retroalimentacionColaboracionAuxiliar = new RetroalimentacionColaboracionAuxiliar();
-            retroalimentacionColaboracionOpt = retroalimentacionColaboracionAuxiliar.getPorPersonaYColaboracion(academicoDTO.getIdPersona(), colaboracionDTO.getIdColaboracion());
-        }
+        RetroalimentacionColaboracionAuxiliar retroalimentacionColaboracionAuxiliar = new RetroalimentacionColaboracionAuxiliar();
+        retroalimentacionColaboracionOpt = retroalimentacionColaboracionAuxiliar.getPorPersonaYColaboracion(academicoDTO.getIdPersona(), colaboracionDTO.getIdColaboracion());
     }
 
     private void cargarLabels () {
@@ -131,6 +129,8 @@ public class ProgresoColaboracionControlador {
                 btnIniciar.setVisible(false);
                 break;
             case enRevision:
+                RetroalimentacionColaboracionAuxiliar retroalimentacionColaboracionAuxiliar = new RetroalimentacionColaboracionAuxiliar();
+                retroalimentacionColaboracionOpt = retroalimentacionColaboracionAuxiliar.getPorPersonaYColaboracion(academicoDTO.getIdPersona(), colaboracionDTO.getIdColaboracion());
                 if (retroalimentacionColaboracionOpt.isPresent()) {
                     btnRetroalimentar.setVisible(false);
                     btnFinalizar.setVisible(true);
